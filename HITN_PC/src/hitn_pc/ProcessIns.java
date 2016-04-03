5c6bc6b2f1c31553a52cd6736e0d1ca7e760e882
Sarthak2119 <saar2119@gmail.com>
2016-04-03 11:29:32 +0530

    almost done

--- HITN_PC/src/hitn_pc/ProcessIns.java
+++ HITN_PC/src/hitn_pc/ProcessIns.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package getme;
+package hitn_pc;
 
 import java.awt.AWTException;
 import java.awt.MouseInfo;
